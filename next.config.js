cf6b10cdc8d7d041d5214672cca08d883738c4bb
lruzicki <lukasz.ruz@gmail.com>
2025-12-21 19:38:20 +0100

    Fix next config ts and js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,16 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ["v0.blob.com", "hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'v0.blob.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+      },
+    ],
     unoptimized: true,
   },
 }
